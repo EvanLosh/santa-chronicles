651238915bead6a05885587d7069988bb3b0a126
Meagan <MeaganTruong1@yahoo.com>
2023-12-04 16:17:08 -0600

    page css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+import Sidebar from './Sidebar';
+import Comments from './Comments';
 
 function App() {
 
@@ -9,8 +13,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
       {/* The Home, About, NewPostForm, and History components get rendered by the router in index.js */}
-
+        <Home />
+        <Sidebar />
+    </div>
+    <Comments />
     </div>
   );
 }
