9f67389baf3ead0ccfbd6d2965c92f34c7c54465
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-04 18:19:42 -0500

    fetch

--- src/History.js
+++ src/History.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-function History() {
-    return <p>This is history.</p>
+function History({ posts }) {
+    return (
+        <div>
+            <p>This is history.</p>
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default History
--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,16 @@ import React from 'react'
 import SelectedPost from './SelectedPost'
 import Sidebar from './Sidebar'
 
-function Home() {
+function Home({ posts }) {
     return (
         <div>
             <p>This is home.</p>
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            ))}
             <SelectedPost />
             <Sidebar />
         </div>
