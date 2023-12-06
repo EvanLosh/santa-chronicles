c6800970eb0062522f1d8a5c5b60975703fd8f7f
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-05 21:39:31 -0500

    search bar

--- src/History.js
+++ src/History.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import SearchBar from './SearchBar';
 
 function History({ posts, displayPost }) {
 
     const navigate = useNavigate() // useNavigate() let's use redirect the user to Home without reloading the page
+    const [searchTerm, setSearchTerm] = useState('')
+    const [filteredPosts, setFilteredPosts] = useState(posts)
+
+    function handleSearch({ searchTerm }) {
+        setSearchTerm(searchTerm)
+
+        const filtered = posts.filter((post) => 
+        post && post.title && post.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        setFilteredPosts(filtered)
+    }
 
     function handleClick(id) {
         displayPost(id)
@@ -12,14 +24,22 @@ function History({ posts, displayPost }) {
 
     return (
         <div>
-            {posts && posts.map((post) => (
+            <SearchBar onSearch={handleSearch} />
+            {searchTerm ? (
+                filteredPosts.map((post) => (
+                    <div key={post.id}>
+                        <h3 onClick={() => handleClick(post.id)}>{post.date} {post.title}</h3>
+                    </div>
+                ))
+            ) : (
+            posts && posts.map((post) => (
                 <div key={post.id}>
                     <h3 onClick={() => handleClick(post.id)}>{post.date} {post.title}</h3>
-
                 </div>
-            ))}
+            ))
+            )}
         </div>
-    )
+    );
 }
 
 export default History
---
+++ src/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    function handleSearch() {
+        onSearch({ searchTerm })
+    }
+
+    return (
+        <div>
+            <input type='text' placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar;
