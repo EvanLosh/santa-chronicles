257ce258961062e0a38133021afed258fc134805
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-07 13:55:30 -0500

    search filter

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,16 +3,22 @@ import React, { useState } from 'react';
 function SearchBar({ onSearch }) {
     const [searchTerm, setSearchTerm] = useState('')
 
-    function handleSearch() {
-        onSearch({ searchTerm })
+    function handleSearchChange(event) {
+        const newSearchTerm = event.target.value;
+        setSearchTerm(newSearchTerm);
+        onSearch({ searchTerm: newSearchTerm });
     }
 
     return (
         <div>
-            <input type='text' placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-            <button onClick={handleSearch}>Search</button>
+            <input
+                type='text'
+                placeholder='Search...'
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
         </div>
-    )
+    );
 }
 
 export default SearchBar;
