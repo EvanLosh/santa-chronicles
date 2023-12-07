53523ebf03f5a5302a2c5a1ea909edf71397da14
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-07 14:34:18 -0500

    css

--- src/History.js
+++ src/History.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import SearchBar from './SearchBar';
+import './History.css';
 
 function History({ posts, displayPost }) {
 
