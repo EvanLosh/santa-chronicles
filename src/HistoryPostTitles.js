74e25210015ba50b10e2ad5930bf060d26640424
Evan Losh <evand.losh@gmail.com>
2023-12-06 20:51:40 -0500

    minor css improvements

--- src/HistoryPostTitles.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function HistoryPostTitles() {
-    return
-}
-
-export default HistoryPostTitles
--- src/RecentPosts.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function RecentPosts() {
-    return
-}
-
-export default RecentPosts
